d9ca80daf7d532ee3f5ee8aa4f9254f56a115bfb
wanghongbo <what.ing@163.com>
2020-01-18 22:34:01 +0800

    初始化

---
+++ TestProject/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TestProject
+//
+//  Created by whating on 2020/1/18.
+//  Copyright © 2020 whating. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by whating on 2020/1/18.
+//  Copyright © 2020 whating. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
